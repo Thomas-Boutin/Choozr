0fef932ead28edcc2b4ab0562d9d742c5b6f19ae
tboutin <tboutin@ippon.fr>
2022-06-04 18:06:28 +0200

    feat: store login parameters upon redirection

--- src/ui/JoinTeam.svelte
+++ src/ui/JoinTeam.svelte
@@ -1,15 +1,24 @@
 <script lang="ts">
-  import { getContext } from "svelte";
+  import { getContext, onMount } from "svelte";
+  import LoginParameters from "../domain/LoginParameters";
   import MemberName from "../domain/MemberName";
   import TeamId from "../domain/TeamId";
   import type JoinTeamUseCase from "../port/input/JoinTeamUseCase";
+  import type LoginUseCase from "../port/input/LoginUseCase";
   import type { JoinTeamScreenParams } from "./JoinTeamScreenParams";
+  import { querystring } from "svelte-spa-router";
 
   export let params: JoinTeamScreenParams;
 
   const teamId = new TeamId(params.teamId);
+  const loginUseCase: LoginUseCase = getContext("loginUseCase");
   const joinTeamUseCase: JoinTeamUseCase = getContext("joinTeamUseCase");
   let memberName = "";
+
+  onMount(() => {
+    const params = new URLSearchParams($querystring);
+    loginUseCase.login(new LoginParameters(params.get("appId")!, params.get("apiKey")!));
+  });
 
   function joinTeam() {
     joinTeamUseCase
